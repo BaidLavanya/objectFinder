d66f93b691473dd5acf71e7f30a6741e0a1b08de
BaidLavanya <75931821+BaidLavanya@users.noreply.github.com>
2022-03-29 15:27:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 modelStatus = "";
+objects = "";
 
 function preload() { }
  
@@ -9,6 +10,11 @@ function setup() {
     webcam.hide();
 }    
 
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
 function modelLoaded() {
     console.log("Model loaded successfully !!");
     modelStatus = true;
@@ -16,4 +22,30 @@ function modelLoaded() {
 
 function draw() {
     image(webcam, 0, 0, 640, 420);
+    if(modelStatus != "") {
+        objectDetector.detect(video, gotResult);
+        for(i=0 ; i < objects.length ; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("noObjs").innerHTML= "No. of objects detected : " + objects.length;
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            fill(r,g,b);
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%" + objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
 }
